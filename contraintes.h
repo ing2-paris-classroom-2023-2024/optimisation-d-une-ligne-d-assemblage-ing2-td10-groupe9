1a4566766192391203f8ce5fb09b976b5f24c0e4
claralealece <131815558+claralealece@users.noreply.github.com>
2023-11-23 13:26:44 +0100

    contraintes.h

---
+++ contraintes.h
@@ -0,0 +1,45 @@
+#ifndef PROJET_CONTRAINTES_H
+#define PROJET_CONTRAINTES_H
+#define MAX_OPERATIONS 35
+
+//c1
+typedef struct {
+    int operation1;
+    int operation2;
+} ExclusionPair;
+//c2
+typedef struct {
+    int matrice[MAX_OPERATIONS][MAX_OPERATIONS];
+} GraphePrecedence;
+//c3
+typedef struct {
+    int matriceExclusions[MAX_OPERATIONS][MAX_OPERATIONS];
+} ContraintesExclusion;
+
+typedef struct {
+    int matricePrecedence[MAX_OPERATIONS][MAX_OPERATIONS];
+} ContraintesPrecedence;
+
+typedef struct {
+    int tempsCycle;
+} ContraintesTempsCycle;
+
+//c1
+void chargerContraintesExclusion(ExclusionPair exclusions[], int* nombreContraintes);
+int estExclue(ExclusionPair exclusions[], int nombreContraintes, int operation1, int operation2);
+//c2
+void initialiserGraphe(GraphePrecedence *graphe);
+void ajouterRelationPrecedence(GraphePrecedence *graphe, int operationPrecedente, int operationSuivante);
+int doitPreceder(GraphePrecedence *graphe, int operationPrecedente, int operationSuivante);
+//c3
+void initialiserContraintesExclusion(ContraintesExclusion *contraintes);
+void initialiserContraintesPrecedence(ContraintesPrecedence *contraintes);
+void initialiserContraintesTempsCycle(ContraintesTempsCycle *contraintes);
+int respecteTempsCycle(ContraintesTempsCycle *contraintes, int dureeTotale);
+void libererMemoireContraintes(ContraintesExclusion *exclusions, ContraintesPrecedence *precedence, ContraintesTempsCycle *tempsCycle);
+
+int chargerTempsCycleMaximal();
+//3
+
+
+#endif //PROJET_CONTRAINTES_H
